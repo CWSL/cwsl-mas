41b12a5137f2b23f52f47867c7ed778fc249ea1e
Tim Bedin <captainceramic@gmail.com>
2014-10-21 14:20:07 +1100

    Removed references to database for the metafile.

--- cwsl/core/metafile.py
+++ cwsl/core/metafile.py
@@ -22,11 +22,7 @@ import os
 
 
 class MetaFiles(object):
-    ''' This object holds the same information that a ClimateFile does,
-
-    but stands in because you can't create ClimateFiles objects without
-    the correct ForeignKeys and the directories will not exist in the
-    db yet.
+    ''' This object represents a single file and it's stored attributes.
 
     '''
 
@@ -37,17 +33,6 @@ class MetaFiles(object):
 
         self.full_path = os.path.join(path_dir, filename)
         self.all_atts = all_atts
-
-    def create_file(self):
-        ''' This method takes the information stored in the MetaClimateFile and
-
-        creates a ClimateFile object that is saved in the database.
-
-        '''
-        pass
-
-        #host_machine, created = HostMachines.objects.get_or_create(name=config.HOST_NAME)
-        #return FileSystemTrawler.create_file_object(self.all_atts, host_machine)
 
     def __repr__(self):
 
