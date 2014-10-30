dbf7d48ded51a5c07e97a8c3e96b7100f1d93d7d
Tim Bedin <captainceramic@gmail.com>
2014-10-30 12:05:15 +1100

    Updated nino34 modules and docs

--- cwsl/init.py
+++ cwsl/init.py
@@ -32,8 +32,7 @@ from cwsl.vt_modules.vt_cdscan import CDScan
 from cwsl.vt_modules.vt_xmltonc import XmlToNc
 from cwsl.vt_modules.vt_seas_vars import SeasVars
 from cwsl.vt_modules.vt_climatology import Climatology
-#from cwsl.vt_modules.vt_nino34 import IndicesNino34
-from cwsl.vt_modules.vt_nino_mon import IndicesNinoMonth
+from cwsl.vt_modules.vt_nino34 import IndicesNino34
 from cwsl.vt_modules.vt_general_command_pattern import GeneralCommandPattern
 from cwsl.vt_modules.vt_constraintbuilder import ConstraintBuilder
 from cwsl.vt_modules.vt_plot_timeseries import PlotTimeSeries
@@ -77,8 +76,7 @@ def initialize(*args, **keywords):
     reg.add_module(Climatology, name='Climatology', namespace='Aggregation')
 
     #Indices
-    #reg.add_module(IndicesNino34, name='Nino3.4', namespace='Indices|Seasonal')
-    reg.add_module(IndicesNinoMonth, name='Nino3.4', namespace='Indices')
+    reg.add_module(IndicesNino34, name='Nino3.4', namespace='Indices')
  
     #Visualisation
     reg.add_module(PlotTimeSeries, name='Plot Timeseries', namespace='Visualisation')
