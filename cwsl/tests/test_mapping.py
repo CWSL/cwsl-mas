5256988a8e73c093b783458f1d5d9f0c82b1c4cb
Tim Bedin <captainceramic@gmail.com>
2014-11-03 16:41:44 +1100

    Added new tests for mapping and looping.

---
+++ cwsl/tests/test_mapping.py
@@ -0,0 +1,73 @@
+"""
+Authors: Tim Bedin, Tim Erwin
+
+Copyright 2014 CSIRO
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Tests for the ProcessUnit 'mapping' API - renaming constraints
+between input and output.
+
+"""
+
+import unittest
+import logging
+
+import mock
+
+from cwsl.configuration import configuration
+from cwsl.core.pattern_dataset import PatternDataSet
+from cwsl.core.process_unit import ProcessUnit
+
+
+module_logger = logging.getLogger('cwsl.tests.test_mapping')
+
+
+class TestMapping(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.input_pattern = '/a/fake/%gcm_model%_%variable%_%file_type%'
+        self.output_pattern = '/a/fake/%gcm_model%_%variable%_%file_type%'
+
+        self.patcher = mock.patch('cwsl.core.pattern_dataset.PatternDataSet.glob_fs')
+        self.mock_globber = self.patcher.start()
+
+        mock_file_1 = '/a/fake/ACCESS1-0_tas_netCDF'
+        mock_file_2 = '/a/fake/MIROC_tas_netCDF'
+        mock_file_3 = '/a/fake/ACCESS1-0_pr_netCDF'
+        
+        self.mock_globber.return_value = [mock_file_1, mock_file_2, mock_file_3]
+
+        # Create a mock pattern dataset to use in process unit tests.
+        self.a_pattern_ds = PatternDataSet(self.input_pattern)
+        
+        # Constant header for the job scripts.
+        self.script_header = "#!/bin/sh\nset -e\n\nmodule purge\nexport CWSL_CTOOLS={}\nexport PYTHONPATH=$PYTHONPATH:{}/pythonlib\n"\
+            .format(configuration.cwsl_ctools_path, configuration.cwsl_ctools_path)
+
+    def tearDown(self):
+
+        self.patcher.stop()
+        
+    def test_simple_mapping(self):
+
+        the_process_unit = ProcessUnit([self.a_pattern_ds], self.output_pattern,
+                                       'echo')
+        
+        ds_result = the_process_unit.execute(simulate=True)
+
+        outfiles = [file_thing for file_thing in ds_result.files]
+        self.assertEqual(len(outfiles), 1)
+
+        expected_string = self.script_header + "mkdir -p /another/file_1\necho test_file1 /another/file_1/pattern_1.txt\n"
+
+
