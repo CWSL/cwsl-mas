4eeee5701b48dac0e655b1c9c77b9ac4d3943d6b
captainceramic <captainceramic@gmail.com>
2015-05-12 14:57:34 +1000

    Added check for missing VisTrail information

--- cwsl/utils/utils.py
+++ cwsl/utils/utils.py
@@ -30,7 +30,7 @@ try:
     import vistrails.api
 except ImportError:
     log.warning("Can not import VisTrails api - is it on the PYTHONPATH?")
-    
+
 
 def get_git_status(ifile):
     """
@@ -40,7 +40,7 @@ def get_git_status(ifile):
     ifile = os.path.expandvars(ifile)
 
     if not os.path.exists(ifile):
-        log.error("{} does not exist!".format(ifile))
+        log.error("Script: {} does not exist on file system".format(ifile))
         git_version = "Could not determine file version."
         return git_version
 
@@ -50,7 +50,7 @@ def get_git_status(ifile):
     try:
         status = subprocess.check_output(['git','status',filename])
         version = subprocess.check_output(['git','log',filename])
-        
+
         if not version:
             log.error("%s not in git repository" % ifile)
             return "Could not determine file version."
@@ -61,11 +61,11 @@ def get_git_status(ifile):
             git_version = "Git info: %s - with uncommited modifications" % version.group(1)
         else:
             git_version = "Git info: %s" % version.group(1)
-    
+
     except subprocess.CalledProcessError:
         log.error("Status called process failed.")
         git_version = "Could not determine file version."
-        
+
     os.chdir(cwd)
 
     return git_version
@@ -75,18 +75,18 @@ def get_vistrails_info():
     """
     Return a tuple with the VisTrails vt file and version information.
 
-    
+
     There is a possible API bug/confusion: This function is failing
     when run in headless / batch mode.
 
     """
-    
+
     try:
         this_controller = vistrails.api.get_current_controller()
     except vistrails.api.NoVistrail:
         # This API call raises a NoVisTrail error in headless mode.
         return (None, None)
-    
+
     filename = this_controller.vistrail.locator.name
     current_version = this_controller.current_version
 
@@ -103,7 +103,7 @@ def build_metadata(command_line_list):
         Current VisTrail, internal pipeline version, git version info
         Script to be run, git version info
         Full command line of the task.
-    
+
     """
 
     rightnow = datetime.now()
@@ -112,17 +112,23 @@ def build_metadata(command_line_list):
 
     vt_info = get_vistrails_info()
 
-    # Get the git information about the script and the vistrails file.
-    vt_git = get_git_status(vt_info[0])
+    if vt_info[0]:
+        log.debug("vt_info is: {}".format(vt_info))
+        # Get the git information about the script and the vistrails file.
+        vt_git = get_git_status(vt_info[0])
+    else:
+        log.warning("No VisTrail internal information found!")
+        vt_git = "No VisTrail infomation found."
+
     script_git = get_git_status(command_line_list[0])
-    
+
     vt_info_list = [short_time, 'VISTRAIL FILE:', str(vt_info[0]), 'NODE NUMBER:', str(vt_info[1]), 'VERSION:', vt_git]
-    
+
     real_script_name = os.path.expandvars(command_line_list[0])
-    full_ver_string = (' '.join([short_time, 'USER:', USER, 'NCI PROJECT:', PROJECT, 'DATE/TIME:', time_string]) + '\n' + 
-                       ' '.join(vt_info_list) + '\n' + 
+    full_ver_string = (' '.join([short_time, 'USER:', USER, 'NCI PROJECT:', PROJECT, 'DATE/TIME:', time_string]) + '\n' +
+                       ' '.join(vt_info_list) + '\n' +
                        ' '.join([short_time, 'SCRIPT:', real_script_name, 'VERSION:', script_git]) + '\n' +
                        ' '.join([short_time, 'CMD:'] + [real_script_name] + command_line_list[1:]) + '\n')
 
     return full_ver_string
-    
+
