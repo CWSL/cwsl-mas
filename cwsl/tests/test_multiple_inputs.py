250033b8fae99654f100de561abe46a9a4fd55d2
captainceramic <captainceramic@gmail.com>
2015-05-28 12:08:27 +1000

    Added two new tests for merging constaints, one that is not yet implemented properly.
    This test tries creates a set of all combinations of inputs with multiple input constraint values.

--- cwsl/tests/test_multiple_inputs.py
+++ cwsl/tests/test_multiple_inputs.py
@@ -67,6 +67,47 @@ class TestMultipleInputs(unittest.TestCase):
 
         self.assertItemsEqual(good_names, all_outs)
 
+    def test_changefile_generation(self):
+        """ This test works to cover the common case when you want to calculate changes.
+
+        For example, comparing two dataset by time.
+        """
+
+        mock_files_1 = ["/model1_1986_rain", "/model2_1986_rain",
+                        "/model3_1986_rain", "/model4_1986_rain",
+                        "/model1_1986_temp"]
+        mock_files_2 = ["/model1_2015_rain", "/model2_2015_rain",
+                        "/model3_2015_rain", "/model4_2015_rain",
+                        "/model1_2015_temp"]
+
+        input_pattern = "/%model%_%date%_%variable%"
+
+        # Create our mock DataSets.
+        with mock.patch('cwsl.core.pattern_dataset.PatternDataSet.glob_fs') as mock_glob:
+            mock_glob.return_value = mock_files_1
+            test_ds_1 = PatternDataSet(input_pattern)
+
+        with mock.patch('cwsl.core.pattern_dataset.PatternDataSet.glob_fs') as mock_glob:
+            mock_glob.return_value = mock_files_2
+            test_ds_2 = PatternDataSet(input_pattern)
+
+        # A ProcessUnit which merges the Constraint on colour.
+        the_process = ProcessUnit([test_ds_1, test_ds_2], "/tmp/%model%_%date%_%variable%",
+                                  "echo", merge_output=["date"])
+
+        output_ds = the_process.execute(simulate=True)
+
+        outfile_names = [metafile.full_path for metafile in output_ds.files]
+
+        expected_files = ["/tmp/model1_1986-2015_rain",
+                          "/tmp/model2_1986-2015_rain",
+                          "/tmp/model3_1986-2015_rain",
+                          "/tmp/model4_1986-2015_rain",
+                          "/tmp/model1_1986-2015_temp"]
+
+        self.assertItemsEqual(expected_files, outfile_names)
+
+
     def test_model_correllation_2(self):
         """ This test is to try combining Constraints from two different DataSets.
 
@@ -99,3 +140,40 @@ class TestMultipleInputs(unittest.TestCase):
         self.assertEqual(len(outfiles), 1)
 
         self.assertEqual(outfiles[0].full_path, "/tmp/echidna_red-blue.file")
+
+    def test_model_correllation_3(self):
+        """ This test is to try combining multiple DataSets, each with many files. """
+
+        mock_files_1 = ["/red_echidna", "/blue_echidna", "/green_echidna"]
+        mock_files_2 = ["/blue_echidna", "/red_echidna", "/green_echidna"]
+
+        input_pattern = "/%colour%_%animal%"
+
+        # Create our mock DataSets.
+        with mock.patch('cwsl.core.pattern_dataset.PatternDataSet.glob_fs') as mock_glob:
+            mock_glob.return_value = mock_files_1
+            test_ds_1 = PatternDataSet(input_pattern)
+
+        with mock.patch('cwsl.core.pattern_dataset.PatternDataSet.glob_fs') as mock_glob:
+            mock_glob.return_value = mock_files_2
+            test_ds_2 = PatternDataSet(input_pattern)
+
+        # A ProcessUnit which merges the Constraint on colour.
+        the_process = ProcessUnit([test_ds_1, test_ds_2], "/tmp/%animal%_%colour%.file",
+                                  "echo", merge_output=["colour"])
+
+        output_ds = the_process.execute(simulate=True)
+
+        outfile_names = [metafile.full_path for metafile in output_ds.files]
+
+        expected_outfiles = ["/tmp/echidna_red-red.file",
+                             "/tmp/echidna_red-blue.file",
+                             "/tmp/echidna_red-green.file",
+                             "/tmp/echidna_blue-red.file",
+                             "/tmp/echidna_blue-blue.file",
+                             "/tmp/echidna_blue-green.file",
+                             "/tmp/echidna_green-red.file",
+                             "/tmp/echidna_green-blue.file",
+                             "/tmp/echidna_green-green.file"]
+
+        self.assertItemsEqual(expected_outfiles, outfile_names)
