39fee42ec43667022d53420315a645e50897f81a
Damien Irving <irving.damien@gmail.com>
2015-05-29 12:47:46 +1000

    Improved documentation

--- cwsl/vt_modules/vt_zonal_agg.py
+++ cwsl/vt_modules/vt_zonal_agg.py
@@ -20,7 +20,6 @@ Part of the CWSLab Model Analysis Service VisTrails plugin.
 
 """
 
-import subprocess
 from vistrails.core.modules import vistrails_module, basic_modules
 
 from cwsl.configuration import configuration
@@ -30,14 +29,26 @@ from cwsl.core.pattern_generator import PatternGenerator
 
 
 class ZonalAggregation(vistrails_module.Module):
-    """This module performs aggregation along the zonal (longitudinal) axis.
+    """Aggregation along the zonal (longitudinal) axis.
 
-    It wraps the cwsl-ctools/aggregation/cdo_zonal_agg.sh script.
+    Wraps the cwsl-ctools/aggregation/cdo_zonal_agg.sh script.
+
+    Inputs:
+      in_dataset: Can consist of netCDF files and/or cdml catalogue files
+      method: Aggregation method. Choices are zonmin, zonmax, zonsum, 
+        zonmean, zonavg, zonvar, zonstd, zonpctl,N 
+        (where N is the percentile)
+    
+    Outputs:
+      out_dataset: Consists of netCDF files (i.e. cdml catalogue files
+      are converted).
 
     """
 
-    _input_ports = [('in_dataset', 'csiro.au.cwsl:VtDataSet'),
-                    ('method', basic_modules.String),
+    _input_ports = [('in_dataset', 'csiro.au.cwsl:VtDataSet', 
+                     {'labels': str(['Input dataset'])}),
+                    ('method', basic_modules.String, 
+                     {'labels': str(['Aggregation method'])}),
                    ]
 
     _output_ports = [('out_dataset', 'csiro.au.cwsl:VtDataSet')]
