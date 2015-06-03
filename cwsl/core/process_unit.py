8dcb75d9f38f01df2747004c5694711ce9fb90c0
captainceramic <captainceramic@gmail.com>
2015-06-03 12:01:09 +1000

    Added a default fallback for the ctools

--- cwsl/core/process_unit.py
+++ cwsl/core/process_unit.py
@@ -276,9 +276,11 @@ class ProcessUnit(object):
 
         """
 
-        # Check that cws_ctools_path is set
+        # Check that cws_ctools_path is set. Drop to a default if necessary.
         if not configuration.cwsl_ctools_path or not os.path.exists(configuration.cwsl_ctools_path):
-            raise Exception("cwsl_ctools_path is not set in package options")
+            module_logger.warning("cwsl_ctools_path is not set in package options")
+        else:
+            configuration.cwsl_ctools_path = "/opt/cwslab-ctools/"
 
         # We now create a looper to compare all the input Datasets with
         # the output FileCreator.
